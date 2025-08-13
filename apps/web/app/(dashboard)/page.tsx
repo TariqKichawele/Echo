8254eb1b24790161cbceca9fb9faa8fcc604c5bf
TariqKichawele <tariqkichawele66@gmail.com>
2025-08-13 10:39:49 -0400

    05: Sentry Integrate Sentry for error tracking in apps/web
    - Added Sentry configuration for client, server, and edge environments.
    - Implemented global error handling to capture exceptions and log them to Sentry.
    - Created example pages and API routes to demonstrate Sentry's error monitoring capabilities.
    - Updated package.json to include Sentry as a dependency.
    - Adjusted next.config.mjs to integrate Sentry's Webpack plugin for source map uploads.

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { OrganizationSwitcher } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const add = useMutation(api.users.add);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <p>Dashboard</p>
       <OrganizationSwitcher hidePersonal={true} />
+      <button onClick={() => add()}>Add</button>
     </div>
   )
 }
